72827082c8e3d3891c0e9423e1913a4b041c511c
Sarthak <pusarthak@gmail.com>
2021-01-24 13:12:29 -0600

    first commit

---
+++ lib/guessing_game.rb
@@ -0,0 +1,41 @@
+class GuessingGame
+    
+    def initialize(min, max)
+        @min = min 
+        @max = max 
+
+        @secret_num = rand(min..max)
+
+        @num_attempts = 0 
+        @game_over = false 
+    end 
+
+    def num_attempts()
+        return @num_attempts 
+    end
+
+    def game_over?()
+        return @game_over 
+    end 
+
+    def check_num(num)
+        @num_attempts += 1 
+
+        if num == @secret_num
+            @game_over = true
+            puts "you win"
+        elsif num > @secret_num 
+            puts "too big"
+        elsif num < @secret_num
+            puts "too small"
+        end 
+    end
+
+    def ask_user()
+        puts "enter a number"
+
+        input = gets.chomp.to_i
+
+        return check_num(input)
+    end 
+end
